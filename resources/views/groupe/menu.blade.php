4ee653f0b57529f95b6bee0c8ebbef4ee3c7f8dc
masangana <alexmasangana@gmail.com>
2023-01-14 23:05:06 +0300

    Fix groupe menu

--- resources/views/groupe/menu.blade.php
+++ resources/views/groupe/menu.blade.php
@@ -11,32 +11,36 @@
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+        <a class="nav-link   @if(!Request()->routeIs('groupe.meute')) collapsed @endif"
+           data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>Unités</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <ul id="components-nav" class="nav-content collapse @if(Request()->routeIs('groupe.meute') || 
+            Request()->routeIs('groupe.troupe') || Request()->routeIs('groupe.compagnie') ||
+            Request()->routeIs('groupe.clan') || Request()->routeIs('groupe.route')) show @endif "
+            data-bs-parent="#sidebar-nav">
           <li>
-            <a href="{{Route('groupe.meute')}}">
+            <a href="{{Route('groupe.meute')}}" class="@if(Request()->routeIs('groupe.meute')) active @endif">
               <i class="bi bi-circle"></i><span>Meute</span>
             </a>
           </li>
           <li>
-            <a href="{{Route('groupe.troupe')}}">
+            <a href="{{Route('groupe.troupe')}}" class="@if(Request()->routeIs('groupe.troupe')) active @endif">
               <i class="bi bi-circle"></i><span>Troupe</span>
             </a>
           </li>
           <li>
-            <a href="{{Route('groupe.compagnie')}}">
+            <a href="{{Route('groupe.compagnie')}}" class="@if(Request()->routeIs('groupe.compagnie')) active @endif">
               <i class="bi bi-circle"></i><span>Compagnie</span>
             </a>
           </li>
           <li>
-            <a href="{{Route('groupe.clan')}}">
+            <a href="{{Route('groupe.clan')}}" class="@if(Request()->routeIs('groupe.clan')) active @endif">
               <i class="bi bi-circle"></i><span>Clan</span>
             </a>
           </li>
           <li>
-            <a href="{{Route('groupe.route')}}">
+            <a href="{{Route('groupe.route')}}" class="@if(Request()->routeIs('groupe.route')) active @endif">
               <i class="bi bi-circle"></i><span>Route</span>
             </a>
           </li>
@@ -44,12 +48,14 @@
       </li><!-- End Components Nav -->
 
       <li class="nav-item">
-        <a class="nav-link " data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+        <a class="nav-link @if(!Request()->routeIs('personnes.create')) collapsed @endif"
+           data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-journal-text"></i><span>Membres</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
-        <ul id="forms-nav" class="nav-content collapse show" data-bs-parent="#sidebar-nav">
+        <ul id="forms-nav" class="nav-content collapse  @if(Request()->routeIs('personnes.create')) show @endif"
+            data-bs-parent="#sidebar-nav">
           <li>
-            <a href=" {{Route('personnes.create')}} ">
+            <a href=" {{Route('personnes.create')}} " class="@if(Request()->routeIs('personnes.create')) active @endif">
               <i class="bi bi-circle"></i><span>Ajouter</span>
             </a>
           </li>
